2d4fd5a3c0df7c59fb5aacd978bc9f73634612b7
ldy2119 <30451868+ldy2119@users.noreply.github.com>
2018-06-05 17:09:21 +0900

    Initial commit

---
+++ app/src/main/java/com/dowellcomputer/webview1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.dowellcomputer.webview1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webview = (WebView) findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webview.loadUrl("https://namu.wiki/w/%EB%82%98%EB%AC%B4%EC%9C%84%ED%82%A4:%EB%8C%80%EB%AC%B8");
+
+    }
+}
